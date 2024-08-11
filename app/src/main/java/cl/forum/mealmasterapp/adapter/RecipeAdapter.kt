daeccda1d92216d83e101a4d80a667bbc02fdd54
matfuentesv <mat.fuentes@duocuc.cl>
2024-08-11 17:21:24 -0400

    avance modulo recetas

---
+++ app/src/main/java/cl/forum/mealmasterapp/adapter/RecipeAdapter.kt
@@ -0,0 +1,34 @@
+package cl.forum.mealmasterapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cl.forum.mealmasterapp.R
+import cl.forum.mealmasterapp.model.Recipe
+
+class RecipeAdapter(private val recipes: List<Recipe>) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_recipe, parent, false)
+        return RecipeViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        val recipe = recipes[position]
+        holder.recipeTitle.text = recipe.title
+        holder.recipeDescription.text = recipe.description
+        // Usar una imagen placeholder predeterminada
+        holder.recipeImage.setImageResource(R.drawable.ic_launcher_foreground)
+    }
+
+    override fun getItemCount() = recipes.size
+
+    class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val recipeTitle: TextView = itemView.findViewById(R.id.recipeTitle)
+        val recipeDescription: TextView = itemView.findViewById(R.id.recipeDescription)
+        val recipeImage: ImageView = itemView.findViewById(R.id.recipeImage)
+    }
+}
---
+++ app/src/main/java/cl/forum/mealmasterapp/model/Recipe.kt
@@ -0,0 +1,7 @@
+package cl.forum.mealmasterapp.model
+
+data class Recipe(
+    val title: String,
+    val description: String,
+    val imageResId: Int
+)
