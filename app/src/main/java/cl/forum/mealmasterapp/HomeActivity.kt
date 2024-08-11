2385cac9273c23d2c433b8ff41716d9d6f412bf3
matfuentesv <mat.fuentes@duocuc.cl>
2024-08-10 23:24:30 -0400

    avance

---
+++ app/src/main/java/cl/forum/mealmasterapp/HomeActivity.kt
@@ -0,0 +1,19 @@
+package cl.forum.mealmasterapp
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+
+    }
+}
